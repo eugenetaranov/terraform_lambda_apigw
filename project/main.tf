9d5d1f54eab3b5e9e928f334c15ab12d9275e06e
Eugene Taranov <eugene@taranov.me>
2020-04-26 00:32:00 +0300

    added vpc

--- project/main.tf
+++ project/main.tf
@@ -37,6 +37,19 @@ module "apigw" {
   lambda_authorizer_invoke_arn      = module.lambda_authorizer.invoke_arn
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.6.0"
+
+  name                 = var.vpc_name
+  cidr                 = var.vpc_cidr
+  azs                  = [for az in var.az : format("%s%s", var.aws_region, az)]
+  public_subnets       = [cidrsubnet(var.vpc_cidr, 9, 0), cidrsubnet(var.vpc_cidr, 9, 1), cidrsubnet(var.vpc_cidr, 9, 2)]
+  private_subnets      = [cidrsubnet(var.vpc_cidr, 9, 10), cidrsubnet(var.vpc_cidr, 9, 11), cidrsubnet(var.vpc_cidr, 9, 12)]
+  enable_nat_gateway   = true
+  enable_dns_hostnames = true
+}
+
 resource "aws_s3_bucket" "storage" {
   bucket_prefix = "storage-"
 }
--- project/variables.tf
+++ project/variables.tf
@@ -1,3 +1,17 @@
 variable "aws_region" {
   default = "us-east-1"
 }
+
+variable "az" {
+  default = ["a", "b", "c"]
+}
+
+variable "vpc_name" {
+  description = "VPC name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC cidr"
+  type        = string
+}
